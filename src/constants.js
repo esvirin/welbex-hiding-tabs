f935e7744c2a16465abf561716dc7096f97825a4
Evgeny Svirin <77930320+esvirin@users.noreply.github.com>
2021-10-30 19:55:06 +0200

    change template

--- src/constants.js
+++
@@ -1,5 +0,0 @@
-define([], function () {
-    return {
-        ROOT_URL: 'https://devcore.kindcode.ru',
-    }
-})
--- src/utils.js
+++
@@ -1,6 +0,0 @@
-define([], function () {
-	return {
-		formatDate: date => new Intl.DateTimeFormat('ru-RU').format(date),
-		isPhone: string => /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/.test(string)
-	}
-})
